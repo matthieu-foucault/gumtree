1c3bcfa964f8c662346c5136eb8f1bd765e7f8c9
Matthieu Foucault <foucault.matthieu@gmail.com>
2015-04-24 08:31:59 -0700

    Modified Java sample 'Example' to have all types of actions in it

--- samples/java/Example_v1.java
+++ samples/java/Example_v1.java
@@ -3,9 +3,8 @@ import java.util.Random;
 public class Example {
 
 	public void hello() {
-		System.out.println("Hello everybody!");
+		System.out.println("Hello everyone!");
 		System.out.println("This code is a magnificent example");
-		System.out.println("For the ASE 2014 conference");
 		System.out.println("It draws a number at random");
 		System.out.println("Adds 10");
 		System.out.println("Multiplies by 10");
